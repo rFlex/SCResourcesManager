084922e7ea65dcea5d8f152de2cea8e995192a36
Simon CORSIN <simon@corsin.me>
2014-08-18 13:11:31 +0200

    Initial Commit

---
+++ SCResourcesManager/SCResourcesManager.h
@@ -0,0 +1,13 @@
+//
+//  SCResourcesManager.h
+//  SCResourcesManager
+//
+//  Created by Simon CORSIN on 18/08/14.
+//  Copyright (c) 2014 Simon Corsin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCResourcesManager : NSObject
+
+@end
---
+++ SCResourcesManager/SCResourcesManager.m
@@ -0,0 +1,13 @@
+//
+//  SCResourcesManager.m
+//  SCResourcesManager
+//
+//  Created by Simon CORSIN on 18/08/14.
+//  Copyright (c) 2014 Simon Corsin. All rights reserved.
+//
+
+#import "SCResourcesManager.h"
+
+@implementation SCResourcesManager
+
+@end
